98dca8a6a7e90a03cd2df011d04ba4f541de9239
AryanTyagi09 <tyagiaryan1318@gmail.com>
2026-05-06 18:23:24 +0530

    Add: stone-game-ii - submission-0

---
+++ Data Structures & Algorithms/stone-game-ii/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int n;
+
+    int solve(vector<int>& piles, int person, int i, int M,
+              vector<vector<vector<int>>>& dp) {
+
+        if (i >= n) return 0;
+
+        if (dp[person][i][M] != -1)
+            return dp[person][i][M];
+
+        int result = (person == 1) ? 0 : INT_MAX;
+        int stones = 0;
+
+        for (int x = 1; x <= min(2*M, n-i); x++) {
+            stones += piles[i+x-1];
+
+            if (person == 1) {
+                result = max(result, stones +
+                    solve(piles, 0, i+x, max(M, x), dp));
+            } else {
+                result = min(result,
+                    solve(piles, 1, i+x, max(M, x), dp));
+            }
+        }
+
+        return dp[person][i][M] = result;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+
+        vector<vector<vector<int>>> dp(
+            2, vector<vector<int>>(n, vector<int>(n+1, -1))
+        );
+
+        return solve(piles, 1, 0, 1, dp);
+    }
+};
+
